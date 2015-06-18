8f56c80909ea97a1c80baab1a38504612f1f5825
Paul Tingle <pt@triarc-labs.com>
2015-06-18 16:39:45 +0200

    Bumped version to 0.0.9

--- tlDisablers.js
+++ tlDisablers.js
@@ -3,13 +3,13 @@ mod.directive("tlAjaxDisable", [
     '$parse',
     function ($parse) { return function (scope, element, attrs) {
         restrict: 'A', 'Click Mousedown Mouseup Mouseover Mouseout Mousemove Mouseenter Mouseleave Keydown Keyup Keypress Submit Focus Blur Copy Cut Paste'.split(' ').forEach(function (action) {
-            var fn = $parse(attrs[attrs.tlAjaxDisable]);
+            var fn = $parse(attrs["ng" + action]);
             if (angular.isFunction(fn) && fn !== angular.noop) {
                 action = action.toLowerCase();
                 element.unbind(action).bind(action, function () {
                     event.preventDefault();
                     var promise = fn(scope, { $event: event });
-                    if (Triarc.hasValue(promise)) {
+                    if (Triarc.hasNoValue(promise)) {
                         element.prop("disabled", true);
                         promise.finally(function () {
                             element.prop("disabled", false);
